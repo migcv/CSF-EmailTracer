c6549c86ad68fa11a6bb072fa8000034c5dd3e47
Miguel <migcviegas@gmail.com>
2016-10-22 12:32:24 +0000

    Improvements on parser

---
+++ scripts.js
@@ -0,0 +1,91 @@
+function parser(){
+    // Gets the email header given by the user
+    var emailHeader = document.getElementById("email-header").value;
+    // Splits the header by '\n'
+    var headerSplitter = emailHeader.split("\n");
+    // Auxiliar variables
+    var j;
+    // Array to store information of each field that matters
+    var deliveredTo = new Array(), from = new Array(), received = new Array(), x_received = new Array(), receivedSPF = new Array(), date = new Array(), subject = new Array(), cc = new Array(), to = new Array();
+
+    for(j = 0; j < headerSplitter.length; j++) {
+
+        if(headerSplitter[j].includes("Delivered-To") && headerSplitter[j].indexOf("Delivered-To") == 0){
+            deliveredTo.push(headerSplitter[j] + "<br>");
+            while(indexOfFields(headerSplitter[j+1]) != 0) {
+                deliveredTo.push(headerSplitter[++j]+ "<br>");
+            }
+        }
+        if(headerSplitter[j].includes("From") && headerSplitter[j].indexOf("From") == 0){
+            from.push(headerSplitter[j] + "<br>");
+            while(indexOfFields(headerSplitter[j+1]) != 0) {
+                from.push(headerSplitter[++j]+ "<br>");
+            }
+        }
+        if(headerSplitter[j].includes("Received") && headerSplitter[j].indexOf("Received") == 0){
+            received.push(headerSplitter[j] + "<br>");
+            while(indexOfFields(headerSplitter[j+1]) != 0) {
+                received.push(headerSplitter[++j]+ "<br>");
+            }
+        }
+        if(headerSplitter[j].includes("X-Received") && headerSplitter[j].indexOf("X-Received") == 0){
+            x_received.push(headerSplitter[j]);
+            while(indexOfFields(headerSplitter[j+1]) != 0) {
+                x_received.push(headerSplitter[++j]+ "<br>");
+            }
+        }
+        if(headerSplitter[j].includes("Received-SPF") && headerSplitter[j].indexOf("Received-SPF") == 0){
+            receivedSPF.push(headerSplitter[j]);
+            while(indexOfFields(headerSplitter[j+1]) != 0) {
+                receivedSPF.push(headerSplitter[++j]+ "<br>");
+            }
+        }
+        if(headerSplitter[j].includes("Date") && headerSplitter[j].indexOf("Date") == 0){
+            date.push(headerSplitter[j]);
+            while(indexOfFields(headerSplitter[j+1]) != 0) {
+                date.push(headerSplitter[++j]+ "<br>");
+            }
+        }
+        if(headerSplitter[j].includes("Subject") && headerSplitter[j].indexOf("Subject") == 0){
+            subject.push(headerSplitter[j]);
+            while(indexOfFields(headerSplitter[j+1]) != 0) {
+                subject.push(headerSplitter[++j]+ "<br>");
+            }
+        }
+        if(headerSplitter[j].includes("Cc") && headerSplitter[j].indexOf("Cc") == 0){
+            cc.push(headerSplitter[j]);
+            while(indexOfFields(headerSplitter[j+1]) != 0) {
+                cc.push(headerSplitter[++j]+ "<br>");
+            }
+        }
+        if(headerSplitter[j].includes("To") && headerSplitter[j].indexOf("To") == 0){
+            to.push(headerSplitter[j]);
+            while(indexOfFields(headerSplitter[j+1]) != 0) {
+                to.push(headerSplitter[++j]+ "<br>");
+            }
+        }
+    }
+    // Sets the result obtained in the respectetive span
+    document.getElementById("deliveredTo").innerHTML = deliveredTo;
+    document.getElementById("from").innerHTML = from;
+    document.getElementById("received").innerHTML = received;
+    document.getElementById("x-received").innerHTML = x_received;
+    document.getElementById("receivedSPF").innerHTML = receivedSPF;
+    document.getElementById("date").innerHTML = date;
+    document.getElementById("subject").innerHTML = subject;
+    document.getElementById("cc").innerHTML = cc;
+    document.getElementById("to").innerHTML = to;
+    // Changes display of #parse-result to "block"
+    document.getElementById("parse-result").style.display = "block";
+}
+
+function indexOfFields(headerLine) {
+    var i;
+    var headerFields = ["Received", "Delivered-To", "X-Received", "Received-SPF", "DKIM-Signature", "Return-Path", "Authentication-Results", "Date", "From", "Reply-To", "Message-ID", "Subject", "MIME-Version", "Content-Type", "To"];
+    for(i = 0; i < headerFields.length; i++) {
+        if(headerLine.indexOf(headerFields[i]) == 0) {
+            return 0;
+        }
+    }
+    return -1;
+}
